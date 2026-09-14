c1f9a4ee4a10ccbd5ae546b29fe0bf1b39736086
bodrovis <elskruk@proton.me>
2026-09-14 23:11:31 +0300

    update const

--- src/constants/files.ts
+++ src/constants/files.ts
@@ -1,2 +1,2 @@
-export const MAX_CSV_FILE_SIZE_BYTES = 25 * 1024 * 1024;
-export const MAX_CSV_FILE_SIZE_LABEL = "25 MB";
+export const MAX_CSV_FILE_SIZE_BYTES = 100 * 1024 * 1024;
+export const MAX_CSV_FILE_SIZE_LABEL = "100 MB";
